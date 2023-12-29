76c6ebe0536b2169bbe11ed021229f9784d13a2d
Jonathan A Elu <veggiedev@hotmail.com>
2023-12-29 20:00:01 +0100

    [FIX] form not looking good in night mode using crispy forms, beggining of change implementation

--- coolegas02/settings.py
+++ coolegas02/settings.py
@@ -101,13 +101,6 @@ DATABASES = {
 
 
 
-#DATABASES = {
-            #'default': {
-            #'ENGINE': 'django.db.backends.sqlite3',
-            #'NAME': BASE_DIR / 'db.sqlite3',
-            #}
-            #}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -144,7 +137,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = 'static/'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
--- theme/static_src/tailwind.config.js
+++ theme/static_src/tailwind.config.js
@@ -42,6 +42,7 @@ module.exports = {
         // '../../**/*.py'
     ],
     theme: {
+
         extend: {},
     },
     plugins: [
