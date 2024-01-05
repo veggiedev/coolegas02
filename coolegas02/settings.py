143e1e69b22cdcb5a5c8a272de8088e87b3c8680
Jonathan A Elu <veggiedev@hotmail.com>
2024-01-05 12:22:49 +0100

    [FIX] fixed issues with database and tested user creating as well as friendship creation

--- coolegas02/settings.py
+++ coolegas02/settings.py
@@ -92,8 +92,8 @@ WSGI_APPLICATION = 'coolegas02.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'django',
-        'PASSWORD': 'django',
+        'NAME': 'postgres',
+        'PASSWORD': 'postgres',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
--- usuarios/models.py
+++ usuarios/models.py
@@ -22,38 +22,38 @@ class Usuario(models.Model):
     def __str__(self):
         return self.user.first_name + " " +  self.user.last_name
 
-class Friends(models.Model):
-    users = models.ManyToManyField(Usuario)
-    current_user=models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='owner')
-
-    @classmethod
-    def make_friend(cls, current_user, new_friend):
-        friend, created = cls.objects.get_or_create(
-            current_user = current_user
-        )
-        friend.users.add(new_friend)
-
-    @classmethod
-    def remove_friend(cls, current_user, new_friend):
-        friend, created = cls.objects.get_or_create(
-            current_user = current_user
-        )
-        friend.users.remove(new_friend)
-
-    def __str__(self):
-        return str(self.current_user)
-
-@login_required
-def add_or_remove_friends(request, username, verb):
-    n_f = get_object_or_404(User, username=username)
-    owner = request.user.userprofile
-    new_friend = UserProfile.objects.get(user=n_f)
-
-    if verb == "add":
-        new_friend.followers.add(owner)
-        Friend.make_friend(owner, new_friend)
-    else:
-        new_friend.followers.remove(owner)
-        Friend.remove_friend(owner, new_friend)
-
-    return redirect(new_friend.get_absolute_url())
+# class Friends(models.Model):
+#     users = models.ManyToManyField(Usuario)
+#     current_user=models.ForeignKey(Usuario, on_delete=models.CASCADE, related_name='owner')
+#
+#     @classmethod
+#     def make_friend(cls, current_user, new_friend):
+#         friend, created = cls.objects.get_or_create(
+#             current_user = current_user
+#         )
+#         friend.users.add(new_friend)
+#
+#     @classmethod
+#     def remove_friend(cls, current_user, new_friend):
+#         friend, created = cls.objects.get_or_create(
+#             current_user = current_user
+#         )
+#         friend.users.remove(new_friend)
+#
+#     def __str__(self):
+#         return str(self.current_user)
+#
+# # @login_required
+# def add_or_remove_friends(request, username, verb):
+#     n_f = get_object_or_404(User, username=username)
+#     owner = request.user.userprofile
+#     new_friend = UserProfile.objects.get(user=n_f)
+#
+#     if verb == "add":
+#         new_friend.followers.add(owner)
+#         Friend.make_friend(owner, new_friend)
+#     else:
+#         new_friend.followers.remove(owner)
+#         Friend.remove_friend(owner, new_friend)
+#
+#     return redirect(new_friend.get_absolute_url())
