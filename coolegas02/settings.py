8a152a28a81607c3bf16d49232de7994958d8c95
veggiedev <veggiedev@hotmail.com>
2024-02-04 22:36:06 +0100

    Update settings.py

--- coolegas02/settings.py
+++ coolegas02/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0)ji9scls1+o(g0-)*!$ulfsxt53e2200t1jbwj0+e1xgnna3z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [www.cool-coolegas.koyeb.app/]
+ALLOWED_HOSTS = ['https://cool-coolegas.koyeb.app/',]
 
 
 # Application definition
@@ -144,7 +144,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = 'theme/static_src/'
+STATIC_ROOT = os.path.join(BASE_DIR, theme/static_src/)
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
