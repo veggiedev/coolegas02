5b7955c51f8ed9a48f23d8240e27c8105d854951
veggiedev <veggiedev@hotmail.com>
2024-02-04 22:24:17 +0100

    Update settings.py

--- coolegas02/settings.py
+++ coolegas02/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0)ji9scls1+o(g0-)*!$ulfsxt53e2200t1jbwj0+e1xgnna3z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [https://cool-coolegas.koyeb.app/]
 
 
 # Application definition
